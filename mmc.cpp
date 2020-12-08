d8cc1433573480938a44ba2c80d457a0f6e16a48
Anuraag Barde <anuraagbarde@gmail.com>
2020-12-08 14:27:29 +0530

    Update mmc.cpp
    fixed 
    1. fact() - added better dynamic programming
    2. fixed p_0
    3. Added invalid case if rho >1 or <0

--- mmc.cpp
+++ mmc.cpp
@@ -1,18 +1,22 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+#include<unordered_map>
 
 using namespace std;
 
 unordered_map<long double, long double> factorial_dp;
 
+
 long double fact(long double x)
 {
+    if(x==0)return 1;
+    if(x==1)return 1;
+    if(x==2)return 2;
+    if(x==3)return 6;
+
     if(factorial_dp.find(x) == factorial_dp.end())
     {
-        long double f=1;
-        for( long double i=1;i<=x; i++)
-        {
-            f = f *i;
-        }
+        long double f = x*fact(x-1);
         factorial_dp[x] = f;
         return f;
     }
@@ -22,26 +26,35 @@ long double fact(long double x)
     }
 }
 
-class mmc
+class mmc 
 {   
-    public:
+    private:
         long double a, s, c;
         long double rho;
         long double p_0 = 0 , p_q = 0;
+        
+    public:
+        bool isvalid = true;
         mmc(long double arr, long double ser, long double c_servers)
         {
             a = arr;
             s = ser;
             c = c_servers;
             rho = a/(c*s);
+
+            if(rho < 0 || rho >= 1)
+            {
+                cout<<"\nThe value of arrival rate and/or service rate is invalid for:";
+                isvalid = false;
+            }
             
             for(long double i =0; i <= (c - 1) ; i++ )
             {
-                p_0 += ((pow(c*rho,i))/(fact(i))) + ((pow(c*rho,c))/(fact(c)*(1-rho)));
+                p_0 += ((pow(c*rho,i))/(fact(i))) ;
             }
+            p_0 += ((pow(c*rho,c))/(fact(c)*(1-rho)));
             
             p_0 = 1/p_0;
-
 
             p_q = (p_0*( pow(c*rho,c)))/(fact(c)*(1-rho));
 
@@ -77,21 +90,26 @@ class mmc
 
 int main()
 {
-    double long arrival = 2;
-    double long service = 2;
-    double long servers = 2;
-    mmc obj = mmc(arrival, service, servers);
+    // for(long double k =1;k<=6;k++)
+    {
+        double long arrival = 2;
+        double long service = 3;
+        double long servers = 2;
+        mmc obj = mmc(arrival, service, servers);
 
-    cout<<"\nArrival rate = "<<arrival<<" | Service rate = "<<service<<" | servers = "<<servers<<"\n";
-    cout<<"Average server utilization : "<<obj.avg_server_util()<<endl;
+        cout<<"\nArrival rate = "<<arrival<<" | Service rate = "<<service<<" | servers = "<<servers<<"\n";
 
-    cout<<"Average customer in queue  : "<<obj.avg_cust_q()<<endl;
+        // if(obj.isvalid == false) continue;
+        cout<<"Average server utilization : "<<obj.avg_server_util()*100<<" %"<<endl;
 
-    cout<<"Average wait in queue      : "<<obj.avg_wait_q()<<endl;
+        cout<<"Average customer in queue  : "<<obj.avg_cust_q()<<endl;
+
+        cout<<"Average wait in queue      : "<<obj.avg_wait_q()<<endl;
+        
+        cout<<"Average customer in system : "<<obj.avg_cust_sys()<<endl;
+
+        cout<<"Average wait in system     : "<<obj.avg_wait_sys()<<endl<<endl;
     
-    cout<<"Average customer in system : "<<obj.avg_cust_sys()<<endl;
-
-    cout<<"Average wait in system     : "<<obj.avg_wait_sys()<<endl<<endl;
-    
+    }
     return 0;
-}
+    }
